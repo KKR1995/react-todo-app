3365c3f17c589c4cbbf1460328e4dacf05dcdb1c
Kanhaiya <kanhaiyakumar1995@outlook.com>
2024-03-02 12:11:36 +0530

    ui updated

--- src/App.js
+++ src/App.js
@@ -20,24 +20,31 @@ function App() {
 
   return (
     <div className="App">
-      <h1>TO-DO APP</h1>
-      <div>
-        <input
-          type="text"
-          placeholder="New Task"
-          value={newTask}
-          onChange={(s) => setNewTask(s.target.value)}
-        />
-        <button onClick={addTask}>Add Task</button>
-      </div>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <button onClick={() => removeTask(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+        <div className="to-do-outer-container">
+            <h1 className="heading">TO-DO APP</h1>
+            <div>
+                <input
+                    className="custom-input-1"
+                    type="text"
+                    placeholder="New Task"
+                    value={newTask}
+                    onChange={(s) => setNewTask(s.target.value)}
+                />
+                <button className="btn_primary"  onClick={addTask}>Add Task</button>
+            </div>
+            <div className="content-box-outer">
+                <div >
+                    <ul className="content-box">
+                        {tasks.map((task, index) => (
+                            <li key={index} className="list-items">
+                                {task}
+                                <button className="link-primary" onClick={() => removeTask(index)}>Remove</button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
